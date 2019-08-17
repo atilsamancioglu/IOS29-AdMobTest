68e651a7aa45e8d6a26c25838827bb5ded389f17
Atil Samancioglu <samancioglu.atil@gmail.com>
2019-08-17 16:57:47 +0300

    first commit

--- GoogleAdMobTest/ViewController.swift
+++ GoogleAdMobTest/ViewController.swift
@@ -7,14 +7,30 @@
 //
 
 import UIKit
+import GoogleMobileAds
 
 class ViewController: UIViewController {
+    
+    var interstitial: GADInterstitial!
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        interstitial = GADInterstitial(adUnitID: "ca-app-pub-3940256099942544/4411468910")
+        let request = GADRequest()
+        interstitial.load(request)
+        
     }
 
-
+    @IBAction func nextClicked(_ sender: Any) {
+      
+        if interstitial.isReady {
+          interstitial.present(fromRootViewController: self)
+        }
+        
+    }
+    
 }
 
---
+++ Podfile
@@ -0,0 +1,12 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'GoogleAdMobTest' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for GoogleAdMobTest
+
+pod 'Google-Mobile-Ads-SDK'
+
+end
